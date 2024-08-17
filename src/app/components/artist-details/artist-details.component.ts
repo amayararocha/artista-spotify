5d8f0e4f84769da92898bae10e35ab99bab885b1
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-08-17 15:29:54 -0300

    task: adicionando mais artistas ao banco e ajustando o componente de detalhes do artista

--- src/app/components/artist-details/artist-details.component.ts
+++ src/app/components/artist-details/artist-details.component.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { ArtistService } from '../../services/artist.service';
 import { ArtistDetails } from '../../models/artist.model';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importando FormsModule
 
 @Component({
   selector: 'app-artist-details',
   templateUrl: './artist-details.component.html',
   styleUrls: ['./artist-details.component.css'],
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor, FormsModule] // Adicionando FormsModule e NgModel aqui
 })
 export class ArtistDetailsComponent implements OnInit {
   artistDetails: ArtistDetails[] = [];
+  filteredArtists: ArtistDetails[] = [];
+  searchTerm: string = '';
 
   constructor(private artistService: ArtistService) {}
 
@@ -19,10 +22,17 @@ export class ArtistDetailsComponent implements OnInit {
     this.artistService.getArtistDetails().subscribe({
       next: (data) => {
         this.artistDetails = data;
+        this.filteredArtists = data;
       },
       error: (err) => {
         console.error('Erro ao buscar detalhes do artista', err);
       }
     });
+  }
+
+  filterArtists(): void {
+    this.filteredArtists = this.artistDetails.filter(artist =>
+      artist.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
