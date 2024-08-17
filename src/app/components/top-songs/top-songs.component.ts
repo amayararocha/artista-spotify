0266249f8ccf85118201c3d63bf3a7491ea8763b
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-08-17 18:35:02 -0300

    task: implementação e estilização de top-songs e ajuste na estilização de popular albums

--- src/app/components/top-songs/top-songs.component.ts
+++ src/app/components/top-songs/top-songs.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistService } from '../../services/artist.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 import { TopSong } from '../../models/artist.model';
 
 @Component({
   selector: 'app-top-songs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './top-songs.component.html',
   styleUrls: ['./top-songs.component.css']
 })
-export class TopSongsComponent {
+export class TopSongsComponent implements OnInit {
+  topSongs: TopSong[] = [];
+  filteredSongs: TopSong[] = [];
+  searchTerm: string = '';
+
+  constructor(private artistService: ArtistService) {}
+
+  ngOnInit(): void {
+    this.artistService.getTopSongs().subscribe({
+      next: (data) => {
+        this.topSongs = data;
+        this.filteredSongs = data;
+      },
+      error: (err) => {
+        console.error('Erro ao buscar músicas', err);
+      }
+    });
+  }
+
+  filterSongs(): void {
+    this.filteredSongs = this.topSongs.filter(song =>
+      song.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
